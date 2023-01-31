39b9675e454fc646a5e928c185dab0dff8654301
goodlifeinc <genata93@gmail.com>
2023-01-31 11:31:28 +0200

    fix: define port

--- examples/14.Ut Run Browser App/ut-user/browser.js
+++ examples/14.Ut Run Browser App/ut-user/browser.js
@@ -1,6 +1,7 @@
 module.exports = () => function utUser() {
     return {
         browser: () => [
+            require('./browser/ui'),
             require('./browser/portal')
         ]
     };
---
+++ examples/14.Ut Run Browser App/ut-user/browser/ui.js
@@ -0,0 +1,9 @@
+module.exports = (...params) => class ui extends require('ut-port-script')(...params) {
+    get defaults() {
+        return {
+            logLevel: 'trace',
+            namespace: ['portal', 'component'],
+            imports: [/\.portal$/, /\.component$/, 'portal', 'component']
+        };
+    }
+};
