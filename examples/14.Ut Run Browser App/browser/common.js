b1b23210c56eedd0a796673089e91d447dc142ea
goodlifeinc <genata93@gmail.com>
2023-01-31 11:31:50 +0200

    fix: simplify config

--- examples/14.Ut Run Browser App/browser/common.js
+++ examples/14.Ut Run Browser App/browser/common.js
@@ -1,5 +1,5 @@
 module.exports = {
-    implementation: 'bdb',
+    implementation: 'testApp',
     repl: false,
     utLog: {
         streams: {
@@ -14,81 +14,5 @@ module.exports = {
             socket: false
         }
     },
-    utLogin: {
-        browser: true
-    },
-    utBrowser: {
-        browser: true,
-        backend: {
-            namespace: [
-                'login',
-                'core',
-                'document',
-                'customer',
-                'permission',
-                'user',
-                'ledger',
-                'policy',
-                'bio',
-                'identity',
-                'rule',
-                'transfer',
-                'report',
-                'bulk',
-                'mirrors',
-                'agent',
-                'notice',
-                'aml',
-                'monitoring',
-                'externalAudit',
-                'history',
-                'content',
-                'plan',
-                'loan',
-                'bdb'
-            ]
-        }
-    },
-    utFrontReact: {browser: true},
-    utPortal: {
-        browser: true,
-        portal: {
-            help: {
-                default: '/a/help/index',
-                'user.user.browse': '/a/help/user.user.browse',
-                'user.role.browse': '/a/help/user.role.browse',
-                'policy.policy.browse': '/a/help/policy.policy.browse'
-            }
-        }
-    },
-    utMicroservice: {browser: true},
-    utCore: {browser: true},
-    utAudit: {browser: true},
-    utHistory: {browser: true},
-    utUser: {browser: true},
-    utCustomer: {browser: true},
-    utDocument: {browser: true},
-    utTransfer: {browser: true},
-    utRule: {browser: true},
-    utAgent: {browser: true},
-    utLedger: {browser: true},
-    utBulk: {browser: true},
-    utNotice: {browser: true},
-    utAml: {browser: true},
-    utReport: {browser: true},
-    utUssd: {browser: true},
-    utCard: {browser: true},
-    utAtm: {browser: true},
-    utPos: {browser: true},
-    utIso: {browser: true},
-    utMirrors: {browser: true},
-    utBdb: {
-        browser: true
-    },
-    utContent: {
-        browser: true
-    },
-    utPlan: {
-        browser: true
-    }
+    utUser: {browser: true}
 };
